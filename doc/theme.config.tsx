daa1f9b157bd497a721dfa29e6059d113bcd7097
Gabriele Bilello <gabriele.bilello1@gmail.com>
2024-06-06 12:09:24 +0200

    doc: Angular brick doc

--- doc/theme.config.tsx
+++ doc/theme.config.tsx
@@ -2,11 +2,18 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 import Logo from './components/Logo';
 
 const config: DocsThemeConfig = {
-  logo: <Logo />,
+  logo: <Logo/>,
   project: {
     link: 'https://github.com/acadevmy/devmy_cli',
   },
   docsRepositoryBase: 'https://github.com/acadevmy/devmy_cli',
+  head: `
+    <link
+      rel="icon"
+      href="/favicon.ico"
+      type="image/x-icon"
+    />
+  `
 }
 
 export default config
